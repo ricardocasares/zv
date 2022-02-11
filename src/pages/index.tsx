ef6f965b2a2898f0bce72064e44e8a8c0f931cf6
Ricardo Casares <84963+ricardocasares@users.noreply.github.com>
2022-02-11 21:52:39 +0100

    docs: fix links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,11 +7,10 @@ const IndexPage = () => (
     <Text kind="h1" weight="bold">zv</Text>
     <Text kind="h2">JSON based resumé</Text>
     <Text kind="h3" family="mono">From devs for devs</Text>
-    <Text>Write your own resume using <a href="https://github.com/jsonresume/resume-schema/blob/master/schema.json">resume.json schema</a> and GitHub.</Text>
+    <Text>Write your own resume using <Link href="https://github.com/jsonresume/resume-schema/blob/master/schema.json">resume.json schema</Link> and GitHub.</Text>
     <Text>Create a new repo called <code>zv</code> and add your <code>zv.json</code> based on the schema.</Text>
-    <Text>
-      Check the sample <Link href="/ricardocasares">ricardocasares</Link>. Make sure you check the <Link href="https://github.com/ricardocasares/zv">source code</Link>.
-    </Text>
+    <Text>Check the sample <Link href="/ricardocasares">ricardocasares</Link>.</Text>
+    <Text><Link href="https://github.com/ricardocasares/zv">GitHub</Link>.</Text>
   </Wrap>
 );
 
