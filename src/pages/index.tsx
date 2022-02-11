d89a87695176fc59b2a83f89cc3d55b3ed0b22e3
Ricardo Casares <84963+ricardocasares@users.noreply.github.com>
2022-02-11 21:34:21 +0100

    docs: update home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,9 +7,10 @@ const IndexPage = () => (
     <Text kind="h1" weight="bold">zv</Text>
     <Text kind="h2">JSON based resumé</Text>
     <Text kind="h3" family="mono">From devs for devs</Text>
-    <Text>Create your own resume using <a href="https://github.com/jsonresume/resume-schema/blob/master/schema.json">resume.json schema</a>.</Text>
+    <Text>Write your own resume using <a href="https://github.com/jsonresume/resume-schema/blob/master/schema.json">resume.json schema</a> and GitHub.</Text>
+    <Text>Create a new repo called <code>zv</code> and add your <code>zv.json</code> based on the schema.</Text>
     <Text>
-      Check the sample <Link href="/ricardocasares">ricardocasares</Link>.
+      Check the sample <Link href="/ricardocasares">ricardocasares</Link>. Make sure you check the <Link href="https://github.com/ricardocasares/zv">source code</Link>.
     </Text>
   </Wrap>
 );
