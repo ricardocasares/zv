02c39211ba37ef60a2c572868ba10fa1578f8608
Ricardo Casares <84963+ricardocasares@users.noreply.github.com>
2022-02-11 21:31:12 +0100

    fix: remove bad words :D

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ const IndexPage = () => (
     <Text kind="h1" weight="bold">zv</Text>
     <Text kind="h2">JSON based resumé</Text>
     <Text kind="h3" family="mono">From devs for devs</Text>
-    <Text>This is a fucking smaller piece of text, some people call it paragraph. That&apos;s a goddamn strange name.</Text>
+    <Text>Create your own resume using <a href="https://github.com/jsonresume/resume-schema/blob/master/schema.json">resume.json schema</a>.</Text>
     <Text>
       Check the sample <Link href="/ricardocasares">ricardocasares</Link>.
     </Text>
